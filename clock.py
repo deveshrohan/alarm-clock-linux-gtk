3c3a76bcd35162d009402ea55762a139211654e1
Devesh Rohan <mail.devrohan@gmail.com>
2015-08-19 00:15:25 +0530

    Clock Fixed [ Two Digit Format ]

--- clock.py
+++ clock.py
@@ -7,9 +7,9 @@ class Clock:
 
     def __get_time__(self):
         current_time = datetime.now().time()
-        hh = str(current_time.hour)
-        mm = str(current_time.minute)
-        ss = str(current_time.second)
+        hh = str("%02d" % current_time.hour)
+        mm = str("%02d" % current_time.minute)
+        ss = str("%02d" % current_time.second)
 
         return hh + ":" + mm + ":" + ss
 
